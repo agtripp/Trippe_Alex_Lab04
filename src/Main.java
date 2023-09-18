728836a2e5f5e9692d0c969a38dfd55ffeafcd35
agtripp <trippeag@mail.uc.edu>
2023-09-18 11:06:28 -0400

    I Commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 2;
+        int intOperandB = 2;
+        int intSum = 2;
+        int intProduct = 2;
+        int intDifference = 2;
+        int intQuotient = 2;
+        int intModulo = 2;
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The remainder of " + intOperandA + " divided by " + intOperandB + " is " + intModulo);
+
+        double doubleOperandA = 3;
+        double doubleOperandB = 3;
+        double doubleSum = 3;
+        double doubleProduct = 3;
+        double doubleDifference = 3;
+        double doubleQuotient = 3;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        doubleSum = doubleOperandA - doubleOperandB;
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        doubleSum = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+    }
+}
+
